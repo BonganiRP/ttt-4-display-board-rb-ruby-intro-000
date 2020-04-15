c6ca523f6682d41758e9e046b66f2893de46fac5
Richard <phalarichard1@gmail.com>
2020-04-15 20:28:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,3 +9,12 @@ def display_board(board)
   puts "   |   |   "
 end
 display_board(board)
+board=[" ", " ", " ", " ", "X", " ", " ", " ", " "]
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board(board)
