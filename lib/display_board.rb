4a8894e007b424493b084f0a4692f84e95a64882
Richard <phalarichard1@gmail.com>
2020-04-15 20:32:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -14,7 +14,7 @@ display_board(board)
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   | X |   "
   puts "-----------"
   puts "   |   |   "
 end
