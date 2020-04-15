b5c80d40bfa7994ef9a695061f36e828d587ad4b
Richard <phalarichard1@gmail.com>
2020-04-15 20:11:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 board=[" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def display_board(board)
   puts "   |   |   "
-  puts "------------"
+  puts "-----------"
   puts "   |   |   "
-  puts "------------"
+  puts "-----------"
   puts "#{board[6]}  | #{board[7]} |#{board[8]}   "
 end
 display_board(board)
